c3296523540e02a873fbd0246edd2988dd1d3683
ranjithstreak <35728127+ranjithstreak@users.noreply.github.com>
2018-03-20 15:49:42 +0530

    Create next number.c

---
+++ next number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a;
+    clrscr();
+    printf("Enter the number:\n");
+    scanf("%d",&a);
+    printf("The next number is %d",a+1);
+    getch();
+    return 0;
+}
